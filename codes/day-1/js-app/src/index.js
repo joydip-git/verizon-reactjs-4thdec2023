dc68714ce5b2c19cd48b136dc405c3db0829b499
Joydip <joydip.mondal@gmail.com>
2023-12-04 13:12:55 +0530

    js/ts update

---
+++ codes/day-1/js-app/src/index.js
@@ -0,0 +1,15 @@
+var strValue = 'JavaScript';
+var intValue = 12;
+var floatValue = 12.34;
+var boolValue = true;
+console.log(typeof strValue, strValue);
+console.log(typeof intValue, intValue);
+console.log(typeof floatValue, floatValue);
+console.log(typeof boolValue, boolValue);
+function add(a, b) {
+    return a + b;
+}
+// function add(a, b) {
+//     return a + b
+// }
+console.log(add(12, 13));
