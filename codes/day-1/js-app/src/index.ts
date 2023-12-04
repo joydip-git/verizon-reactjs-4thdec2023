48e294ba9565cb475940d973ccf0d8de44abdfba
Joydip <joydip.mondal@gmail.com>
2023-12-04 17:03:25 +0530

    ts and new fetaures update

--- codes/day-1/js-app/src/index.ts
+++ codes/day-1/js-app/src/index.ts
@@ -1,49 +1,8 @@
-//function main(){
-//type inference
-/*
-var strValue = 'JavaScript'
-var intValue = 12
-var floatValue = 12.34
-var boolValue = true
-console.log(typeof strValue, strValue)
-console.log(typeof intValue, intValue)
-console.log(typeof floatValue, floatValue)
-console.log(typeof boolValue, boolValue)
-
-function add(a: number, b: number): number {
-    return a + b
+interface MathOps<T, TResult> {
+    add(a: T, b: T): TResult;
 }
-console.log(add(12, 13))
-*/
-
-//console.log(x)//? undefined
-var m = 100
-function call() {
-    //this.y=20
-    var x
-    x = 10 //let, const
-    console.log(x)//? 10
-    for (var i = 0; i < 1; i++) {
-        var x
-        x = 20
-        console.log(x)//? 20
-    }
-    console.log(x)//? 10 or 20
-
-    //function declaration
-    inner()
-    function inner() {
-        console.log('inner')
-    }
-    inner()
-
-    //another()
-    //function expression
-    var another
-    another = function () {
-        console.log('another')
+class MathOperations implements MathOps<number, number> {
+    add(a: number, b: number): number {
+        return a + b
     }
 }
-call()
-//}
-//main()
