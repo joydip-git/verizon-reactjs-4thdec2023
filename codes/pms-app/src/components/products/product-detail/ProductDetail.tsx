f0a5c3ba09c68c231c2250a55605921fd3510965
Joydip <joydip.mondal@gmail.com>
2023-12-07 17:16:14 +0530

    Update ProductDetail.tsx

--- codes/pms-app/src/components/products/product-detail/ProductDetail.tsx
+++ codes/pms-app/src/components/products/product-detail/ProductDetail.tsx
@@ -1,6 +1,30 @@
+import { Link, useParams, useNavigate } from "react-router-dom"
+
+type ParamsType = {
+    id: string
+}
 const ProductDetail = () => {
+    const navigate = useNavigate()
+    const params = useParams<ParamsType>()
+    let id = '';
+    if (params.id) {
+        id = params.id
+    }
     return (
-        <div>ProductDetail</div>
+        <div>
+            {/* <Link to={`/products/edit/${id}`}>
+                <button type="button" className="btn btn-primary">Edit</button>
+            </Link> */}
+
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={
+                    () => navigate(`/products/edit/${id}`)
+                }>
+                Edit
+            </button>
+        </div>
     )
 }
 
